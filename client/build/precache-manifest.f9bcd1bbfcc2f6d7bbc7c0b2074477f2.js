1511cce200fafcb485b4fcd9e6259187c5eaff00
Collin Betori <cbetori@virtusre.com>
2019-08-23 15:47:51 -0500

    Server added path to static files

---
+++ client/build/precache-manifest.f9bcd1bbfcc2f6d7bbc7c0b2074477f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6624eec14248d731cf995c23b979b8a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3b3153bfd7c88881c2d",
+    "url": "/static/css/2.676dc204.chunk.css"
+  },
+  {
+    "revision": "dac94a2c4123b530924a",
+    "url": "/static/css/main.2cacb7fd.chunk.css"
+  },
+  {
+    "revision": "a3b3153bfd7c88881c2d",
+    "url": "/static/js/2.c46a0a36.chunk.js"
+  },
+  {
+    "revision": "dac94a2c4123b530924a",
+    "url": "/static/js/main.671a1c56.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
