8620a54f65998a6bd6a29e9d0fa3a16b971dfc7e
Collin Betori <cbetori@virtusre.com>
2019-08-23 15:49:16 -0500

    Server added path to static files

--- server.go
+++ server.go
@@ -7,17 +7,50 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 
 	"github.com/gorilla/mux"
 	_ "github.com/joho/godotenv/autoload"
 )
 
 //Model
+type spaHandler struct {
+	staticPath string
+	indexPath  string
+}
 
 //Controller
 func apiHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the HomePage!")
 	fmt.Println("Endpoint Hit: homePage")
+}
+
+func (h spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// get the absolute path to prevent directory traversal
+	path, err := filepath.Abs(r.URL.Path)
+	if err != nil {
+		// if we failed to get the absolute path respond with a 400 bad request
+		// and stop
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	// prepend the path with the path to the static directory
+	path = filepath.Join(h.staticPath, path)
+
+	// check whether a file exists at the given path
+	_, err = os.Stat(path)
+	if os.IsNotExist(err) {
+		// file does not exist, serve index.html
+		http.ServeFile(w, r, filepath.Join(h.staticPath, h.indexPath))
+		return
+	} else if err != nil {
+		// if we got an error (that wasn't that the file doesn't exist) stating the
+		// file, return a 500 internal server error and stop
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	// otherwise, use http.FileServer to serve the static dir
+	http.FileServer(http.Dir(h.staticPath)).ServeHTTP(w, r)
 }
 
 func main() {
@@ -29,6 +62,9 @@ func main() {
 	// db.ConnectionString()
 	db.Connection()
 	// Route Handlers / Endpoints
+	spa := spaHandler{staticPath: "client/build", indexPath: "index.html"}
+	r.PathPrefix("/").Handler(spa)
+
 	r.HandleFunc("/api", apiHandler).Methods("GET")
 
 	r.HandleFunc("/api/funds", controller.FundsHandler).Methods("GET")
